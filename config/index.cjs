3ed00ee3b2d4cfea65a3b0a4f4a6d0fb106f703c
yuanyuanlele <72909302+yuanyuanlele@users.noreply.github.com>
2024-07-22 19:31:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'orOdc69O_oQrpaQ4eAMz7EBzcRL0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '_nDpMKhAZLNWqVEBVMPDAfkwfpCW_xOjT44BWJgkrU4',
+      useTemplateId: 'kGpCEMuk0eccPwC7Rpr2hAe7mHUj7RK_gs84-cSizlk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-04',
       festivals: [
